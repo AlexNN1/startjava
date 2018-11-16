c494b08b4832648fe17916291ef47f0eb06fe706
AlexN <mailalexn@gmail.com>
2018-11-17 01:27:34 +0300

    Renew

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        byte myFootwearSize = 43; //Размер обуви
+        short myGrowth = 174; //Рост
+        int myAgeFullYears = 45; //Полных лет
+        long myPopulationCity = 45000; //Население города
+        float myWeight = (float) 85.54; //Вес
+        double myExactAge = 45.11; //Точный возраст
+        char myGender = 'М'; //Пол
+        boolean check = true;
+
+        System.out.println("Размер обуви " + myFootwearSize);
+        System.out.println("Рост " + myGrowth);
+        System.out.println("Полных лет " + myAgeFullYears);
+        System.out.println("Население города " + myPopulationCity);
+        System.out.println("Вес " + myWeight);
+        System.out.println("Точный возраст " + myExactAge);
+        System.out.println("Пол " + myGender);
+        System.out.println("" + check);
+    }
+}
