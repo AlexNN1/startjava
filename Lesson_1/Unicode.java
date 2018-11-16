b158de507a69192c82232bb8425162fd71803c89
AlexN <mailalexn@gmail.com>
2018-11-17 01:12:36 +0300

    Renew

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,14 @@
+public class Unicode {
+    public static void main (String[] args) {
+
+        char[] ca = {'\u9398'};
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 32; j++) {
+                String sc = new String(ca);
+                System.out.print(sc + " ");
+                ca[0]++;
+            }
+            System.out.println();
+        }
+    }
+}
