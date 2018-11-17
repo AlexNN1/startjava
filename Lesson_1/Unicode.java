522f3aa01fb7aaf6a061b590d342eabb94af0587
AlexN <mailalexn@gmail.com>
2018-11-17 14:20:05 +0300

    New Unicode

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,12 +1,13 @@
 public class Unicode {
     public static void main (String[] args) {
 
-        char code = '\u9398';
+char code = 9398; // Ⓐ
         for (int i = 0; i < 24; i++) {
             System.out.println(code + " ");
             for (int j = 0; j < 32; j++) {
                 System.out.print(code + " ");
                 code++;
             }
+        }
     }
 }
