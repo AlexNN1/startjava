fe88579915c49ffa98747c1aac09a5322c47c125
AlexN <mailalexn@gmail.com>
2018-11-24 22:32:41 +0300

    Add Calculator

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,38 +1,34 @@
 public class Calculator {
 
-    int calculate(int num1, int num2, char operations) {
-        int num3 = num1;
-        int num4 = num2;
-        char operation = operations;
+    int calculate(int num1, int num2, char operation) {
 
-        int expo = 1;
+        int exponent = 1;
         int result = 0;
         switch (operation) {
             case '+':
-                result = num3 + num2;
+                result = num1 + num2;
                 break;
             case '-':
-                result = num3 - num2;
+                result = num1 - num2;
                 break;
             case '*':
-                result = num3 * num2;
+                result = num1 * num2;
                 break;
             case '/':
-                result = num3 / num2;
+                result = num1 / num2;
                 break;
             case '^':
                 if (num2 > 0) {
                     for (int i = 1; i <= num2; i++) {
-                        expo *= num3;
+                        exponent *= num1;
                     }
                 }
-                result = expo;
+                result = exponent;
                 break;
             case '%':
-                result = num3 % num2;
+                result = num1 % num2;
             default:
-//                КодВыбораПоУмолчанию;
-                break;
+                System.out.println("The opertion symbol is not correctly entered, repeat input");
         }
         return result;
     }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,15 +2,20 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main (String[] args) {
+
         Calculator calculatorOne = new Calculator();
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Enter the first number: ");
         int num1 = scan.nextInt();
+
         System.out.print("Enter the second number: ");
         int num2 = scan.nextInt();
+
         System.out.print("Enter of mathematical operation: ");
-        char operations = scan.next().charAt(0);
-        int result1 = calculatorOne.calculate(num1, num2, operations);
+        char operation = scan.next().charAt(0);
+
+        int result1 = calculatorOne.calculate(num1, num2, operation);
         System.out.println(result1);
     }
 }
