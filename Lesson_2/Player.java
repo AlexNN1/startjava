1e8561b09f9a16d7eac25f465d61eeca296188f5
AlexN <mailalexn@gmail.com>
2018-11-28 17:50:08 +0300

    Add Player.java

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -13,7 +13,7 @@ public class Player {
             } if (num2 > n) {
                 System.out.println("Number Player2 high number PC");
                 System.out.println("Player2 = " + num2);
-                num1--;
+                num2--;
             } if (num1 < n) {
                 System.out.println("Number Player1 down number PC");
                 System.out.println("Player1 = " + num1);
@@ -21,7 +21,7 @@ public class Player {
             } if (num2 < n) {
                 System.out.println("Number Player2 down number PC");
                 System.out.println("Player2 = " + num2);
-                num1++;
+                num2++;
             } if (num1 == n) {
                 System.out.println("Player1 won");
                 System.out.println("Player1 = " + num1);
